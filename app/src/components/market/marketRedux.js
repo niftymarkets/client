2d963fc0188132f1e3190f0dd22eb73be3ad2979
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2019-03-11 10:54:28 +0000

    Write dummy data

---
+++ app/src/components/market/marketRedux.js
@@ -0,0 +1,33 @@
+const dummyItemData = [
+  {
+    itemId: 1,
+    name: 'Love Ranger',
+    price: 10,
+    description: 'Aim for the heart.',
+    category: 'outfits',
+    seller: '1',
+    img_url: 'https://cdn.thetrackernetwork.com/cdn/fortnite/93062_large.png',
+    availability: 'available'
+  },
+  {
+    itemId: 2,
+    name: 'Floss',
+    price: 29,
+    description: 'Express yourself on the battlefield.',
+    category: 'emotes',
+    seller: '2',
+    img_url:
+      'https://cdn.thetrackernetwork.com/cdn/fortnite/9AB75723_large.png',
+    availability: 'available'
+  },
+  {
+    itemId: 3,
+    name: 'Stop Axe',
+    price: 40,
+    description: 'Never stop axin',
+    category: 'outfits',
+    seller: '3',
+    img_url: 'https://cdn.thetrackernetwork.com/cdn/fortnite/93062_large.png',
+    availability: 'available'
+  }
+]
