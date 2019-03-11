e4c26c84dbfb1961fb60ffeca55f69e30cc7caea
Pav0l <pavol.karas@gmail.com>
2019-03-11 09:14:51 +0100

    Create folder structure and templates for redux actions

---
+++ app/src/actions/actionCreators.js
@@ -0,0 +1,27 @@
+import * as types from './actionTypes';
+
+
+// actionCreator template
+export const actionName = (args) => dispatch => { // rename function accordingly
+  dispatch(onError(null));
+  dispatch(onLoad(true));
+
+  // axios function here
+  // .then(res => dispatch(onFetch(res.data)))
+  //   .catch(err => dispatch(onError(err.message)))
+  //   .finally(() => dispatch(onLoad(false)));
+};
+
+export const onLoad = bool => {
+  return ({
+    type: types.LOADING,
+    payload: bool,
+  });
+}
+
+export const onError = err => {
+  return ({
+    type: types.ERROR,
+    payload: err,
+  });
+}
