ea2dca33f9bc6772e691f4151e370c89adc7ccba
delbaoliveira <32464864+delbaoliveira@users.noreply.github.com>
2019-03-11 13:27:38 +0000

    Render market items

--- app/src/components/market/MarketWrapper.jsx
+++ app/src/components/market/MarketWrapper.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import Gallery from './Gallery'
 
 class MarketWrapper extends Component {
   render() {
-    return <div>I'm the MarketWrapper</div>
+    const { gameItems } = this.props
+    return (
+      <main>
+        <Gallery gameItems={gameItems} />
+      </main>
+    )
   }
 }
 
-export default MarketWrapper
+const mapStateToProps = state => {
+  return {
+    gameItems: state.gameItems
+  }
+}
+
+export default connect(mapStateToProps)(MarketWrapper)
