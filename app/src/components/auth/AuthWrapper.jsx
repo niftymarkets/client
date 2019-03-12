ef9939d110cf9f383eacfb6c5a564be84be7aa82
Pav0l <pavol.karas@gmail.com>
2019-03-12 09:57:36 +0100

    Create Login form component and links

--- app/src/components/auth/AuthWrapper.jsx
+++ app/src/components/auth/AuthWrapper.jsx
@@ -1,8 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Login from './Login';
 
 class AuthWrapper extends Component {
   render() {
-    return <div>I'm the AuthWrapper</div>
+    return (
+    <div>
+      <Login />
+    </div>
+    );
   }
 }
 
