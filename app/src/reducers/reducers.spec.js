62d4a1774bf07fb6cc0e1b445787285ef01886f7
Velsu <riftmage50@gmail.com>
2019-03-15 00:23:15 +0100

    change the mockUser to reflect new data, add wishlist test

--- app/src/reducers/reducers.spec.js
+++ app/src/reducers/reducers.spec.js
@@ -6,52 +6,9 @@ describe("user reducer", () => {
     userDetails: {},
     wishList: [],
     userItems: [],
-
     transactionHistory: {
-      boughtItems: [
-        {
-          itemId: 6,
-          name: "Floss",
-          price: 29,
-          description: "Express yourself on the battlefield.",
-          category: "emotes",
-          buyerId: 1,
-          userId: 2,
-          username: "same",
-          img_url:
-            "https://cdn.thetrackernetwork.com/cdn/fortnite/9AB75723_large.png",
-          availability: 0
-        },
-        {
-          itemId: 19,
-          name: "Squirtle",
-          price: 100,
-          description: "One part squirrel, one part turtle",
-          category: "pets",
-          buyerId: 1,
-          userId: 9,
-          username: "quinn",
-          img_url:
-            "https://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/500px-007Squirtle.png",
-          availability: 0
-        }
-      ],
-
-      soldItems: [
-        {
-          itemId: 2,
-          name: "Cuddle Team Leader",
-          price: 25,
-          description: "Hug it out.",
-          category: "outfits",
-          buyerId: 3,
-          userId: 1,
-          username: "scott",
-          img_url:
-            "https://cdn.thetrackernetwork.com/cdn/fortnite/22163_large.png",
-          availability: 0
-        }
-      ]
+      boughtItems: [],
+      soldItems: []
     },
 
     addItem: {
@@ -67,5 +24,46 @@ describe("user reducer", () => {
 
   it("should return the initial state", () => {
     expect(reducers.user(undefined, {})).toEqual(mockUser);
+  });
+
+  it("should update wishlist", () => {
+    expect(
+      reducers.user(mockUser, {
+        type: types.GET_WISHLIST,
+        payload: [
+          {
+            wishlistId: 1,
+            sellerId: 10,
+            itemId: 20,
+            name: "Magikarp"
+          }
+        ]
+      })
+    ).toEqual({
+      userDetails: {},
+      wishList: [
+        {
+          wishlistId: 1,
+          sellerId: 10,
+          itemId: 20,
+          name: "Magikarp"
+        }
+      ],
+      userItems: [],
+      transactionHistory: {
+        boughtItems: [],
+        soldItems: []
+      },
+
+      addItem: {
+        name: "",
+        price: "",
+        description: "",
+        category: "Outfits", // this is the default value for dropdown
+        userId: "",
+        img_url: "",
+        availability: ""
+      }
+    });
   });
 });
