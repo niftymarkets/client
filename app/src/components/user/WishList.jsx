d8bcf5c4fbf2c81e463b2eb5f73be09418d3d419
Pav0l <pavol.karas@gmail.com>
2019-03-14 20:30:09 +0100

    style wishlist

--- app/src/components/user/WishList.jsx
+++ app/src/components/user/WishList.jsx
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import Wish from './Wish'
 
 class WishList extends Component {
   render() {
     return (
-      <div>
+      <WishListContainer>
         <h6>Your Wishlist:</h6>
-        <ul>
-          {this.props.wishList.map(wish => (
+        <StyledList>
+          {
+            this.props.wishList.length < 1 ?
+              <li>You have no items in your wishlist</li>
+            :
+            this.props.wishList.map(wish => (
             <Wish key={wish.wishlistId} wish={wish} />
           ))}
-        </ul>
-      </div>
+        </StyledList>
+      </WishListContainer>
     )
   }
 }
 
 export default WishList
+
+const WishListContainer = styled.div`
+
+  h6 {
+    margin-bottom: 0.5rem;
+  }
+`
+
+const StyledList = styled.ul`
+  border: 1px solid #212b38;
+  border-radius: 4px;
+  padding: 0.5rem;
+  min-width: 350px;
+`
