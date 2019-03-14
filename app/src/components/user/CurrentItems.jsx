ea08a8bbd0bf1fc11dd8c2574832f1729525200e
Pav0l <pavol.karas@gmail.com>
2019-03-14 21:02:58 +0100

    Style user items list

--- app/src/components/user/CurrentItems.jsx
+++ app/src/components/user/CurrentItems.jsx
@@ -1,28 +1,45 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
 import ItemForm from './AddItemForm'
 import ItemCard from '../common/ItemCard'
 
 class CurrentItems extends Component {
   render() {
-    if (!this.props.userItems) {
-      return <div>Loading user items...</div>
+    if (this.props.userItems.length < 1) {
+      return <LoadingDiv>Loading user items...</LoadingDiv>
     }
 
     return (
-      <div>
+      <UserItemsContainer>
         <p>Add new item here!</p>
         <ItemForm />
         <div>
           <h6>Your current items:</h6>
-          <div>
+          <ItemsContainer>
             {this.props.userItems.map(item => (
               <ItemCard key={item.itemId} item={item} hasDeleteButton={true} />
             ))}
-          </div>
+          </ItemsContainer>
         </div>
-      </div>
+      </UserItemsContainer>
     )
   }
 }
 
 export default CurrentItems
+
+
+const LoadingDiv = styled.h6`
+  color: #fcfcfc;
+  margin: 1rem auto;
+`
+const UserItemsContainer = styled.div`
+  margin: 0 2rem;
+`
+
+const ItemsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 1rem -1rem;
+`
+
